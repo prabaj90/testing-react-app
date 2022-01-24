374e12d6afa469fe65d5fa367b5004bc8fa53fab
prabaj90 <prabaj90@gmail.com>
2022-01-24 22:02:13 +0530

    initial commit

---
+++ src/components/__tests__/todo.test.js
@@ -0,0 +1,23 @@
+import{render, screen, cleanup} from '@testing-library/react';
+import Todo from '../todo';
+
+afterEach(()=>{
+    cleanup();
+});
+test('should render the non-completed todo ', ()=>{
+    const todo =  {id:1, title:"wash dishes", completed: false};
+    render(<Todo todo={todo}/>);
+    const todoElement = screen.getByTestId('todo-1');
+    expect(todoElement).toBeInTheDocument();
+    expect(todoElement).toHaveTextContent('wash dishes');
+    expect(todoElement).not.toContainHTML('<strike>');
+});
+
+test('should render the completed todo ', ()=>{
+    const todo =  {id:2, title:"wash car", completed: true};
+    render(<Todo todo={todo}/>);
+    const todoElement = screen.getByTestId('todo-2');
+    expect(todoElement).toBeInTheDocument();
+    expect(todoElement).toHaveTextContent('wash car');
+    expect(todoElement).toBeInTheDocument('<strike>');
+});
